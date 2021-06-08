9456b5ea6efe9666ffcad49284c345e059850044
Emma Hoffman <hoffmane24@greenhill.org>
2021-06-08 11:15:14 -0500

    first commit

---
+++ Menu.py
@@ -0,0 +1,37 @@
+#Emma Hoffman
+#06/08/21
+
+def menu():
+    print("**********************")
+print ("My Game")
+print ("Menu")
+print("[1] Level 1")
+print("[2] Level 2")
+print("[3] Print Scores")
+print ("[4] Exit Game")
+
+
+option = int(input("Enter your option: "))
+while option!= 0:
+    if option == 1:
+        #do option 1 things
+        print("Option 1 has been chosen")
+    elif option == 2:
+        #do option 2 things
+        print("Option 2 has been chosen")
+    elif option == 3:
+        #do option 3 things
+        print("Option 3 has been chosen")
+    elif option == 4:
+        #do option 4 things
+        
+        print("Invalid Option")
+
+
+print()
+menu()
+option = int(input("Enter your option"))
+print("Good bye")
+
+
+
