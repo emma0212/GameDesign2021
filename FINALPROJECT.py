9f7ecffc397b471c6dbca9c22bf3432cf1ef0794
Emma Hoffman <hoffmane24@greenhill.org>
2021-06-25 10:42:41 -0600

    last day commit

--- FINALPROJECT.py
+++ FINALPROJECT.py
@@ -1,5 +1,5 @@
 #Emma Hoffman
-#Creating a solitaire game using pygame
+#Creating a simon says game using pygame
 import os,random,time,sys,pygame
 pygame.init()
 os.system('cls')
@@ -45,58 +45,57 @@ WIDTH=1000
 HEIGHT=900
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 
-#loop to keep window open
-def menu():
-    check=True
-    while check:
-        #Creating title
-        screen=pygame.display.set_mode((WIDTH,HEIGHT))
-        pygame.display.set_caption("Simon Says")
-        screen.fill(GREY)
-        text = TitleFont.render("MENU", 1, BLACK)
-        screen.blit(text, (440,15))
-        #level 1
-        text=CharacterFont.render("Level 1",1,WHITE)
-        screen.blit(text,(422,100))
-        #rectangle 1=level 1
-        rect1=pygame.Rect(340, 100, Wbox*2,Hbox)
-        pygame.draw.rect(screen, WHITE, rect1, width=4)
-        text = TitleFont.render("  ", 1, BLACK)
-        screen.blit(text, (160, 350))
-        #level 2
-        text=CharacterFont.render("Level 2",1,WHITE)
-        screen.blit(text,(422,200))
-        #rectangle 2=level 2
-        rect2=pygame.Rect(340, 200, Wbox*2,Hbox)
-        pygame.draw.rect(screen, WHITE, rect2, width=4)
-        text = TitleFont.render("  ", 1, BLACK)
-        screen.blit(text, (160, 450))
-        #level 3
-        text=CharacterFont.render("Level 3",1,WHITE)
-        screen.blit(text,(422,300))
-        #rectangle 3=level 3
-        rect3=pygame.Rect(340, 300, Wbox*2,Hbox)
-        pygame.draw.rect(screen, WHITE, rect3, width=4)
-        text = TitleFont.render("  ", 1, BLACK)
-        screen.blit(text, (160, 450))
-        #scoreboard
-        text=CharacterFont.render("Scoreboard",1,WHITE)
-        screen.blit(text,(390,400))
-        #rectangle 4= scoreboard
-        rect4=pygame.Rect(340, 400, Wbox*2,Hbox)
-        pygame.draw.rect(screen, WHITE, rect4, width=4)
-        text = TitleFont.render("  ", 1, BLACK)
-        screen.blit(text, (160, 450))
-        # Exit
-        text=CharacterFont.render("Exit Game",1,WHITE)
-        screen.blit(text,(400,500))
-        #rectangle 5= exit game
-        rect5=pygame.Rect(340, 500, Wbox*2,Hbox)
-        pygame.draw.rect(screen, WHITE, rect5, width=4)
-        text = TitleFont.render("  ", 1, BLACK)
-        screen.blit(text, (160, 450))
-        pygame.time.delay(2000)
-        pygame.display.update()
+
+check=True
+while check:
+    #Creating title
+    screen=pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption("Simon Says")
+    screen.fill(GREY)
+    text = TitleFont.render("MENU", 1, BLACK)
+    screen.blit(text, (440,15))
+    #level 1
+    text=CharacterFont.render("Level 1",1,WHITE)
+    screen.blit(text,(422,100))
+    #rectangle 1=level 1
+    rect1=pygame.Rect(340, 100, Wbox*2,Hbox)
+    pygame.draw.rect(screen, WHITE, rect1, width=4)
+    text = TitleFont.render("  ", 1, BLACK)
+    screen.blit(text, (160, 350))
+    #level 2
+    text=CharacterFont.render("Level 2",1,WHITE)
+    screen.blit(text,(422,200))
+    #rectangle 2=level 2
+    rect2=pygame.Rect(340, 200, Wbox*2,Hbox)
+    pygame.draw.rect(screen, WHITE, rect2, width=4)
+    text = TitleFont.render("  ", 1, BLACK)
+    screen.blit(text, (160, 450))
+    #level 3
+    text=CharacterFont.render("Level 3",1,WHITE)
+    screen.blit(text,(422,300))
+    #rectangle 3=level 3
+    rect3=pygame.Rect(340, 300, Wbox*2,Hbox)
+    pygame.draw.rect(screen, WHITE, rect3, width=4)
+    text = TitleFont.render("  ", 1, BLACK)
+    screen.blit(text, (160, 450))
+    #scoreboard
+    text=CharacterFont.render("Scoreboard",1,WHITE)
+    screen.blit(text,(390,400))
+    #rectangle 4= scoreboard
+    rect4=pygame.Rect(340, 400, Wbox*2,Hbox)
+    pygame.draw.rect(screen, WHITE, rect4, width=4)
+    text = TitleFont.render("  ", 1, BLACK)
+    screen.blit(text, (160, 450))
+    # Exit
+    text=CharacterFont.render("Exit Game",1,WHITE)
+    screen.blit(text,(400,500))
+    #rectangle 5= exit game
+    rect5=pygame.Rect(340, 500, Wbox*2,Hbox)
+    pygame.draw.rect(screen, WHITE, rect5, width=4)
+    text = TitleFont.render("  ", 1, BLACK)
+    screen.blit(text, (160, 450))
+    pygame.time.delay(2000)
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -116,21 +115,17 @@ def menu():
                 print("Goodbye. Thank you for playing!") 
                 pygame.quit()
                 sys.exit()
-pygame.display.update()
+        pygame.display.update()
 
 #defining what happens when buttons clicked
-
-def Buttons():
-    startx=round((WIDTH - (Wbox + dist) * 13) / 2)
-    starty=400
-    
-class Button:
+class Button():
     def __init__(self, color, pos):
         self.color = color
         self.pos = pos
         self.shape = pygame.Rect
         self.draw()
 
+#change colors of squares when pressed
 def darken(self):
     index = 0
     for i in self.color:
@@ -156,18 +151,14 @@ def darken_then_lighten(self):
     pygame.time.delay(1000)
     self.lighten()
 
+#Creating buttons
+
+
 #creating squares for game
 r_square=Button(RED,(200,20,60))
 g_square=Button(GREEN,(38,230,0))
 b_square=Button(BLUE,(25,25,255))
 y_square=Button(YELLOW,(255,255,0))
 pygame.display.update()
-
-
-
-
-
-
-
 
 
